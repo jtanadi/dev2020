bb441b8f944804e40220e7933be35e016eceecd8
Jesen Tanadi <mail@jesentanadi.com>
2020-01-11 22:27:11 -0500

    add css-loader

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  webpack: function(config) {
+  webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
+    }, {
+      test: /\.css$/,
+      use: 'css-loader',
     })
     return config
   },
