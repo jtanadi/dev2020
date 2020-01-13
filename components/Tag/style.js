abea57ca8d44dfcf8dade760e7660e50f8a0a2bf
Jesen Tanadi <mail@jesentanadi.com>
2020-01-12 19:30:53 -0500

    fix styling

--- components/Tag/style.js
+++ components/Tag/style.js
@@ -2,10 +2,11 @@ import css from "styled-jsx/css"
 
 export default css`
 div {
+  font-size: 0.75rem;
   display: inline-block;
   width: auto;
-  padding: 0.5rem;
-  margin: 0.5rem 0.5rem 0.5rem 0;
+  padding: 0.45rem 0.75rem;
+  margin: 0.5rem 0.5rem 0 0;
   background-color: lightgray;
 }
 
@@ -17,15 +18,23 @@ div:last-child {
   background-color: #F0DB4F;
 }
 
-.tag-react {
-  background-color: #61DAFB;
-}
-
 .tag-node {
   background-color: #68A063;
 }
 
+.tag-psql {
+  background-color: #007bff;
+}
+
+.tag-react {
+  background-color: #61DAFB;
+}
+
 .tag-redux {
   background-color: #764abc;
+}
+
+.tag-python {
+  background-color: #2b5b84;
 }
 `
