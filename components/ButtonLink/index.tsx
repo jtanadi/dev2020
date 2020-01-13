1fc114d2f3f1ff2e2bc2f3d8432b12f37aa61ac7
Jesen Tanadi <mail@jesentanadi.com>
2020-01-12 19:28:59 -0500

    wrap in flex div, add target prop

--- components/ButtonLink/index.tsx
+++ components/ButtonLink/index.tsx
@@ -6,15 +6,17 @@ export interface LinkInterface {
   name: string
 }
 
-export default function ButtonLink(props: { link: LinkInterface }) {
-  const { link } = props
+export default function ButtonLink(props: { link: LinkInterface, target?: string }) {
+  const { link, target } = props
   return (
     <>
-      <a href={ link.url } target="_blank">
-        <button>
-          { link.name }
-        </button>
-      </a>
+      <div className="button-container">
+        <a href={ link.url } target={ target ? target : "_blank" }>
+          <button>
+            { link.name }
+          </button>
+        </a>
+      </div>
 
       <style jsx>{ style }</style>
     </>
--- components/ButtonLink/style.js
+++ components/ButtonLink/style.js
@@ -1,20 +1,43 @@
 import css from "styled-jsx/css"
 
 export default css`
+.button-container {
+  flex: 1;
+  margin: 0.5rem 0.5rem 0 0;
+  max-width: 33.3%;
+  position: relative;
+}
+
+.button-container:last-child {
+  margin-right: 0;
+}
+
+/*
 button {
-  display: inline-block;
-  width: 120px;
+  width: 100%;
   height: 40px;
   color: white;
-  margin: 0.5rem 0.5rem 0 0;
   padding: 0.5rem;
-  background-color: dodgerblue;
   border: none;
   border-radius: 5px;
   cursor: pointer;
 }
+*/
+
+button {
+  width: 100%;
+  height: 40px;
+  padding: 0.5rem;
+  border: none;
+  color: white;
+  background-color: dodgerblue;
+  box-shadow: 0px 0px 8px lightgray;
+  cursor: pointer;
+}
 
 button:hover {
+  color: dodgerblue;
+  background-color: white;
   box-shadow: 0px 0px 8px dodgerblue;
 }
 `
